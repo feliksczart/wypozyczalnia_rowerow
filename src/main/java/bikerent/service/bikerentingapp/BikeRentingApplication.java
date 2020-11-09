c7c289b2a6a6111fbfa59b811a8283ccc44f5730
piter104 <piotr.derenowski@gmail.com>
2020-11-09 15:34:21 +0100

    initial commit

---
+++ src/main/java/bikerent/service/bikerentingapp/BikeRentingApplication.java
@@ -0,0 +1,13 @@
+package bikerent.service.bikerentingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BikeRentingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BikeRentingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/bikerent/service/bikerentingapp/BikeRentingApplicationTests.java
@@ -0,0 +1,13 @@
+package bikerent.service.bikerentingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BikeRentingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
